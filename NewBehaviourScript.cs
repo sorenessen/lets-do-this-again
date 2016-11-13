d4628a2531fb79d290e4ff2372779461fdd94994
soren essen <soren essen>
2016-11-12 20:21:50 -0500

    asdf Signed-off-by: soren essen <soren essen>

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+    public Transform[] backgrounds;                 //Array(list) of all the back and foregrounds to be paralaxed
+    private float[] parallaxScales;                  //Propertion of the camera movement by which to move the backgrounds
+    public float smoothing = 1f;                    //How smooth the paralax is going to be. Make sure to set this above '0'
+
+    private Transform cam;                          //Reference to the main camera's transform
+    private Vector3 previousCamPos;                 //The position of the camera in the previous frame
+
+    //is called before Start. Great for references.
+    void Awake()
+    {
+        //setup the camera reference
+        cam = Camera.main.transform;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        //The previous frame had the current camera's position
+        previousCamPos = cam.position;
+
+        //assigning corresponding parallax scales
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //for each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            //the parallax is opposite of the camera movement because the previous frame multiplied by the scale
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+            //set a target x position which is the current position plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            //create a target position which is the backgrounds current position with its target x position
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            //fade between current position and target position using what is called 'lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+        //set the previousCamPos to the camera's position at the end of the frame
+        previousCamPos = cam.position;
+    }
+}
---
+++ Tiling.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+public class Tiling : MonoBehaviour {
+
+    public int offsetX = 2;                 //the offset so we don't get any weird errors
+
+    //these are used for checking to see if we need to instantiate stuff
+    public bool hasARightBuddy = false;
+    public bool hasALeftBuddy = false;
+    //used if the object is not tileable
+    public bool reverseScale = false;
+    //this is the width of our element
+    private float spriteWidth = 0f;
+
+    private Camera cam;
+    private Transform myTransform;
+
+    void Awake () {
+        cam = Camera.main;
+        myTransform = transform;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //does it still need buddies? If not, do nothing...
+        if (hasALeftBuddy == false || hasARightBuddy == false) {
+            //calculate the cameras extent (half the widtch) of what camera can see in world coordinates
+            float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+            //calculate the x position of where the camera can see the edge of the sprite (or element)
+            float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth / 2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (myTransform.position.x - spriteWidth / 2) + camHorizontalExtend;
+            //checking if we can see the edge of the element and then calling MakeNewBuddy if we can
+            if (cam.transform.position.x >= edgeVisiblePositionRight - offsetX && hasARightBuddy == false) {
+                MakeNewBuddy(1);
+                hasARightBuddy = true;
+            }else if(cam.transform.position.x <= edgeVisiblePositionLeft + offsetX && hasALeftBuddy == false) {
+                MakeNewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+        }
+	}
+    //a function that creates a new buddy on the side required
+    void MakeNewBuddy (int rightOrLeft) {
+        //calculating the new position for our new buddy and then calling MakeNewBuddy if we can
+        Vector3 newPosition = new Vector3(myTransform.position.x + spriteWidth * rightOrLeft, myTransform.position.y, myTransform.position.z);
+        //Instantiating our new buddy and storing him in a new variable
+        Transform newBuddy = Instantiate(myTransform, newPosition, myTransform.rotation) as Transform;
+        //if not tileable, let's reverse the x size of our object to get rid of ugly seams
+        if (reverseScale == true) {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x * -1, newBuddy.localScale.y, newBuddy.localScale.z);
+
+        }
+
+        newBuddy.parent = myTransform.parent;
+        if (rightOrLeft > 0){
+            newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+
+        }
+        else {
+            newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+
+        }
+    }
+
+}
